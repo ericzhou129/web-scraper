10576f14cebdf55e2c765f5f1d27867b19755c7e
Eric <ericzhou1994@gmail.com>
2017-02-22 01:42:20 +0000

    Changed persistency to boolean

--- scrape.py
+++ scrape.py
@@ -3,7 +3,7 @@ import requests
 payload = {
 	"username": "Daik1226",
 	"password": "Daik1226",
-	"isPersistent": "false"
+	"isPersistent": 0
 	}
 
 #login_url= "https://connect.just-eat.ca/login?client_id=RappsOrigPartnerCentre&response_type=code&redirect_uri=https:%2F%2Fpartner.just-eat.ca%2Fsignin-jeconnect&state=Vch_AK7_0-moIIZIe84tKXdDhdDJBMd2JKAxNYiS5KA2Ax9s7YhMf7wmeFTPItHRfZIPpecD7YjZIY8N7ZghkHTIFZxttdDhjTR5TDAvtCf32lMGoY4SLlBTMc6qc46wusjX3jKemq2IITRmMBkxzVykaxludg03bUnI2xD1JwwcJqrzOrfRP1yDWQZ40B8gKGIe--gXWiDV-z16IOGvbv0wGO8"
