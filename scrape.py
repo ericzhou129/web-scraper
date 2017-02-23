a2c49c1acff5670b5c0d76d031a04c493df10325
ericzhou129 <ericzhou1994@gmail.com>
2017-02-23 11:23:19 -0800

    Web scraper working file -- up to using beautiful soup to find order links Still needs to figure out best method to extract the href links

--- scrape.py
+++ scrape.py
@@ -27,9 +27,9 @@ login_url = 'https://connect.just-eat.ca/api/account/login'
 s = requests.session()
 s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
 
-result = s.post(login_url, data=payload)
-print(result.status_code)
-MYCOOKIES = result.cookies
+r = s.post(login_url, data=payload)
+print(r.status_code)
+MYCOOKIES = r.cookies
 
 # result = s.get('https://partner.just-eat.ca/', cookies=MYCOOKIES)
 # print(result.status_code)
@@ -42,7 +42,11 @@ request_date_url = "https://partner.just-eat.ca/Orders/OrdersBetweenDates/" + da
 result = s.get(request_date_url, cookies=MYCOOKIES)
 print(result.status_code)
 
-#Function which uses beaufiful soup to find href (order) links and store them into an ____?
+#STEP 3: using beautifulsoup, compile a list of order links.
 
-
-#For each index page within a search execute the bs function
+# #use bs4 to find all order links
+result_soup = BeautifulSoup(result.content, 'html.parser')
+#print(result_soup.prettify())
+odd = result_soup.find_all('a', href = True, class_= 'odd orderDetailsRow')
+#^^THIS SYNTAX NEEDS TO BE FIXED
+print(odd)
